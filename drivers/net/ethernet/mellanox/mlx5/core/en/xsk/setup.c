8114a366b2918b8259f4f26db4aac9f02fc37b09
Maxim Mikityanskiy <maximmi@nvidia.com>
2021-01-18 16:31:49 +0200

    net/mlx5e: Add activate/deactivate stage to XDPSQ
    Normally, the lifecycle of a queue in mlx5e has four transitions: open,
    activate, deactivate and close. However, XDPSQs combine open with
    activate and close with deactivate. This patch cleans it up by properly
    separating these stages in order to make it possible to move
    synchronize_net in the following fix.
    
    Fixes: 0a06382fa406 ("net/mlx5e: Encapsulate open/close queues into a function"
    Signed-off-by: Maxim Mikityanskiy <maximmi@nvidia.com>
    Signed-off-by: Lama Kayal <lkayal@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/xsk/setup.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/xsk/setup.c
@@ -171,7 +171,7 @@ void mlx5e_close_xsk(struct mlx5e_channel *c)
 void mlx5e_activate_xsk(struct mlx5e_channel *c)
 {
 	set_bit(MLX5E_RQ_STATE_ENABLED, &c->xskrq.state);
-	/* TX queue is created active. */
+	mlx5e_activate_xdpsq(&c->xsksq);
 
 	spin_lock_bh(&c->async_icosq_lock);
 	mlx5e_trigger_irq(&c->async_icosq);
@@ -180,6 +180,6 @@ void mlx5e_activate_xsk(struct mlx5e_channel *c)
 
 void mlx5e_deactivate_xsk(struct mlx5e_channel *c)
 {
+	mlx5e_deactivate_xdpsq(&c->xsksq);
 	mlx5e_deactivate_rq(&c->xskrq);
-	/* TX queue is disabled on close. */
 }
